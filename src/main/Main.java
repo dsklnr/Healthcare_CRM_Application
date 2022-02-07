6b82cbdb3b8f37f02c1bfc0e3cf19b6dd4335353
BenOgle <benogle1@gmail.com>
2022-02-06 17:48:19 -0700

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import helper.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        JDBC.openConnection();
+
+        System.out.println("Hello world from client");
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainScreen.fxml"));
+        stage.setTitle("Main Screen");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
