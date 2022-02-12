dc1ba92ff34bb11e19881e1ea13346bf1bd1252e
BenOgle <benogle1@gmail.com>
2022-02-11 17:08:52 -0700

    02/11/2022 5:08pm

--- src/controller/MainScreenController.java
+++ src/controller/MainScreenController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -23,10 +24,10 @@ public class MainScreenController implements Initializable {
     public void onLogin(javafx.event.ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/DashboardScreen.fxml"));
         Parent root = loader.load();
-        Stage stage = new Stage();
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         stage.close();
         stage.setTitle("CRM Dashboard");
-        stage.setScene(new Scene(root, 1000, 600));
+        stage.setScene(new Scene(root, 1500, 800));
         stage.show();
     }
 }
